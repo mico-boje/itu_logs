2a79d8065e9940aecddea2f304ee34a44827bc3d
Mico Bøje <mico.boeje@gmail.com>
2023-02-18 22:18:11 +0100

    load balancer

--- data_generator/data_generator/app/main.py
+++ data_generator/data_generator/app/main.py
@@ -1,24 +1,37 @@
 import random
 
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from data_generator.database.data_loader import DataLoader
 from data_generator.data.log_parser import LogParser
 
 
+
+ids = []
 app = FastAPI()
-data_loader = DataLoader()
-ids = data_loader.get_ids()
-if len(ids) == 0:
-    log_parser = LogParser()
-    log_parser()
-    ids = data_loader.get_ids()
+
+def get_data_loader() -> DataLoader:
+    data_loader = DataLoader()
+    return data_loader
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
+@app.post("/utils/db_init")
+def parse_log(data_loader: DataLoader = Depends(get_data_loader)):
+    global ids
+    if len(ids) == 0:
+        log_parser = LogParser()
+        log_parser()
+        ids = data_loader.get_ids()
+    return "Database initialized"
+
+
 @app.get("/logs/get_record")
-def get_record():
+def get_record(data_loader: DataLoader = Depends(get_data_loader)):
+    global ids
+    if len(ids) == 0:
+        ids = data_loader.get_ids()
     return data_loader.get_log_message(random.choice(ids)).log_message
